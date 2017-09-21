fbcbc3044719e480fa8091934b0266aabc06613d
squebler <squebler@gmail.com>
2017-09-20 23:40:21 -0600

    Initial commit of PathfinderCGAutomation with simple character shuffler.

---
+++ PathfinderCGAutomation/src/CharacterShuffler.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by tomas on 9/20/2017.
+ */
+public class CharacterShuffler {
+
+    public static void main(String[] args) {
+
+        List<Character> chars = new ArrayList<>();
+        chars.add('a');
+        chars.add('b');
+        chars.add('c');
+        chars.add('d');
+        chars.add('e');
+
+        List<Character> shuffled = shuffle(chars);
+
+        System.out.print("Shuffled: ");
+        for (Character c : shuffled) {
+            System.out.print(c + ", ");
+        }
+        System.out.println();
+    }
+
+    public static List<Character> shuffle(List<Character> src) {
+        Random rand = new Random();
+
+        List<Character> shuffled = new ArrayList<>(src.size());
+
+        while (!src.isEmpty()) {
+            int srcInd = rand.nextInt(src.size());
+            Character c = src.remove(srcInd);
+            shuffled.add(c);
+        }
+
+        return shuffled;
+    }
+}
