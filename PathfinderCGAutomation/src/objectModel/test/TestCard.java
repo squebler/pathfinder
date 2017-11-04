e8f01ae3e8ac4cecc676b08d10cf397f7cb816fd
squebler <squebler@gmail.com>
2017-11-04 00:04:42 -0600

    Added one missing unit test for Card.

--- PathfinderCGAutomation/src/objectModel/test/TestCard.java
+++ PathfinderCGAutomation/src/objectModel/test/TestCard.java
@@ -2,6 +2,7 @@ package objectModel.test;
 
 import objectModel.Card;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by tomas on 10/8/2017.
@@ -11,5 +12,7 @@ public class TestCard {
     @Test
     public void testConstructor_validNameIn_noException() {
         Card c = new Card("test-card");
+
+        assertEquals("test-card", c.getName());
     }
 }
