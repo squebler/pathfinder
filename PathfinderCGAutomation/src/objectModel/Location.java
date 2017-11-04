221429a02216b5312a86050b1cd9a58d51f283a5
squebler <squebler@gmail.com>
2017-11-04 01:07:10 -0600

    Added initial Location class. It doesn't really do anything yet, and there's no tests.

---
+++ PathfinderCGAutomation/src/objectModel/Location.java
@@ -0,0 +1,42 @@
+package objectModel;
+
+/**
+ * Created by tomas on 11/4/2017.
+ */
+public class Location extends Card {
+
+    private int numMonsters;
+    private int numBarriers;
+    private int numWeapons;
+    private int numSpells;
+    private int numArmors;
+    private int numItems;
+    private int numAllies;
+    private int numBlessings;
+    private String whileAbility;
+    private String closingChallenge;
+    private String permCloseEffect;
+    private boolean isAbyssal;
+    // Front side vs back side
+    // Image
+    // Lore
+
+    public Location(String name, int numMonsters, int numBarriers, int numWeapons, int numSpells, int numArmors,
+                    int numItems, int numAllies, int numBlessings, String whileAbility, String closingChallenge,
+                    String permCloseEffect, boolean isAbyssal) {
+        super(name);
+
+        this.numMonsters = numMonsters;
+        this.numBarriers = numBarriers;
+        this.numWeapons = numWeapons;
+        this.numSpells = numSpells;
+        this.numArmors = numArmors;
+        this.numItems = numItems;
+        this.numAllies = numAllies;
+        this.numBlessings = numBlessings;
+        this.whileAbility = whileAbility;
+        this.closingChallenge = closingChallenge;
+        this.permCloseEffect = permCloseEffect;
+        this.isAbyssal = isAbyssal;
+    }
+}
